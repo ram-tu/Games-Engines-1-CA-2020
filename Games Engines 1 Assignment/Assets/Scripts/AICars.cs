e5de97d9de8444507c4b45154004d45a6661beb9
ram-tud <46999658+ram-tu@users.noreply.github.com>
2020-11-09 16:08:09 +0000

    AI car added

---
+++ Games Engines 1 Assignment/Assets/Scripts/AICars.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AICars : MonoBehaviour
+{
+    public int speed;
+
+    private int currentSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSpeed = speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0,0,speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Car"))
+        {
+            speed = 0;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Car"))
+        {
+            speed = currentSpeed;
+        }
+    }
+}
