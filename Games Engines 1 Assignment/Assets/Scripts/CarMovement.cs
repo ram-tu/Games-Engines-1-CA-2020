540974eaa79a6143a18094ce4a65ea7a6b3ce341
ram-tud <46999658+ram-tu@users.noreply.github.com>
2020-11-09 16:40:47 +0000

    Sky and move up and down

--- Games Engines 1 Assignment/Assets/Scripts/CarMovement.cs
+++ Games Engines 1 Assignment/Assets/Scripts/CarMovement.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class CarMovement : MonoBehaviour
 {
     public int speed;
+
     public int rotationSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -17,8 +19,13 @@ public class CarMovement : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        
-        transform.Translate(Vector3.forward * (speed*Time.deltaTime * vertical));
-        transform.Rotate(Vector3.up * (rotationSpeed*Time.deltaTime * horizontal));
+
+        transform.Translate(Vector3.forward * (speed * Time.deltaTime * vertical)); 
+        transform.Rotate(Vector3.up * (rotationSpeed * Time.deltaTime * horizontal)); 
+
+        if (Input.GetKey(KeyCode.P))
+            transform.Rotate(Vector3.left * (rotationSpeed * Time.deltaTime));
+        if(Input.GetKey(KeyCode.L))
+            transform.Rotate(Vector3.right * (rotationSpeed * Time.deltaTime));
     }
 }
