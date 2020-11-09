ec11e7c507a6a784a79a214da3da3e07fc6ce79b
ram-tud <46999658+ram-tu@users.noreply.github.com>
2020-11-09 15:40:09 +0000

    Movement and camera

---
+++ Games Engines 1 Assignment/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public int speed;
+    public int rotationSpeed;
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position, speed * Time.deltaTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation,target.rotation,rotationSpeed * Time.deltaTime);
+    }
+}
---
+++ Games Engines 1 Assignment/Assets/Scripts/CarMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    public int speed;
+    public int rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        
+        transform.Translate(Vector3.forward * (speed*Time.deltaTime * vertical));
+        transform.Rotate(Vector3.up * (rotationSpeed*Time.deltaTime * horizontal));
+    }
+}
