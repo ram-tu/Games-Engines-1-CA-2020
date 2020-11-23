a81f06e14b759f633912cf2c9a55e62a8c0a1fea
ram-tud <46999658+ram-tu@users.noreply.github.com>
2020-11-23 20:24:36 +0000

    traffic light script

---
+++ Games Engines 1 Assignment/Assets/Scripts/TrafficLight.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficLight : MonoBehaviour
+{
+    private GameObject[] lights = new GameObject[3];
+
+    public int timeFrame;
+
+    public int startingLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            lights[i] = transform.GetChild(i + 1).gameObject;
+        }
+        //lights[startingLight].GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+        StartCoroutine(ChangeLight());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Awake()
+    {
+        //StartCoroutine(ChangeLight());
+    }
+
+    IEnumerator ChangeLight()
+    {
+        while (true)
+        {
+            lights[(startingLight) % 3].GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+            lights[(startingLight + 1) % 3].GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
+            startingLight++;
+            yield return new WaitForSeconds(timeFrame);
+        }
+    }
+}
