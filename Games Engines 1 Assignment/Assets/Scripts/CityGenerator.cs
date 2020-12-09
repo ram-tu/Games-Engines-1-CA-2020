6d75b1d32e50567fb76cdeb0a0ed8903a9b348f8
ram-tud <46999658+ram-tu@users.noreply.github.com>
2020-12-09 09:34:23 +0000

    refactoring

--- Games Engines 1 Assignment/Assets/Scripts/CityGenerator.cs
+++ Games Engines 1 Assignment/Assets/Scripts/CityGenerator.cs
@@ -93,34 +93,35 @@ public class CityGenerator : MonoBehaviour
                 Vector3 pos = new Vector3(player.transform.position.x+(w * buildingSpacing),0,player.transform.position.z + (h * buildingSpacing));
                 if (noise < -2)
                 {    
-                    //string objName = "Cross Road " + ((int) (pos.x)).ToString() + " " + ((int) (pos.z)).ToString();
+                    string tilename = "Road" + ((int)(pos.x)).ToString() + "_" + ((int)(pos.z)).ToString();
                     GameObject crossRoads = Instantiate(crossRoad, new Vector3(pos.x,-0.499f,pos.z), Quaternion.identity).gameObject;
-                    //crossRoads.name = objName;
+                    crossRoads.name = tilename;
                     CityObj obj = new CityObj(updateTime,crossRoads);
-                    //cityObj[objName] = obj;
+                    cityObj[tilename] = obj;
                     cityObjects.Add(crossRoads);
-                   // cityObj.Add(objName,obj);
+                    //cityObj.Add(tilename,obj);
                     
 
                 }
                 else if (noise < -1)
                 {
-                    //string objName = "Road back " + ((int) (pos.x)).ToString() + " " + ((int) (pos.z)).ToString();
+                    string tilename = "Road" + ((int)(pos.x)).ToString() + "_" + ((int)(pos.z)).ToString();
+                  
                     GameObject roadbacks = Instantiate(roadBack, new Vector3(pos.x,-0.499f,pos.z), roadBack.transform.rotation).gameObject;
-                   // roadbacks.name = objName;
+                    roadbacks.name = tilename;
                     CityObj obj = new CityObj(updateTime,roadbacks);
-                    //cityObj[objName] = obj;
+                    cityObj[tilename] = obj;
                     cityObjects.Add(roadbacks);
                     //cityObj.Add(objName,obj);
                 }
                 else if (noise < 0)
                 {
-                    //string objName = "Road Front " + ((int) (pos.x)).ToString() + " " + ((int) (pos.z)).ToString();
+                    string tilename = "Road" + ((int)(pos.x)).ToString() + "_" + ((int)(pos.z)).ToString();
                     GameObject roadfronts = Instantiate(roadFront, new Vector3(pos.x,-0.499f,pos.z), Quaternion.identity).gameObject;
                     cityObjects.Add(roadfronts);
-                    //roadfronts.name = objName;
+                    roadfronts.name = tilename;
                     CityObj obj = new CityObj(updateTime,roadfronts);
-                    //cityObj[objName] = obj;
+                    cityObj[tilename] = obj;
                     //cityObj.Add(objName,obj);
                 }
                 else if (noise < 2)
@@ -143,19 +144,20 @@ public class CityGenerator : MonoBehaviour
                 {
                     choice = 4;
                 }
-
                 if (choice < 10)
                 {
-                    GameObject buildingC = Instantiate(buildings[choice], pos, Quaternion.identity).gameObject;
-                    cityObjects.Add(buildingC);
-                    cityObjects.Add(buildingC);
-                    //buildingC.name = Name;
-                    CityObj buildingss = new CityObj(updateTime,buildingC);
-                }
-                //string Name = "Building 5 " + ((int) (pos.x)).ToString() + " " + ((int) (pos.z)).ToString();
+                    string tilename = "Building" + ((int)(pos.x)).ToString() + "_" + ((int)(pos.z)).ToString();
+                    GameObject building = Instantiate(buildings[choice], pos, Quaternion.identity).gameObject;
+                    cityObjects.Add(building);
+                    cityObjects.Add(building);
+                    building.name = tilename;
+                    CityObj newBuilding = new CityObj(updateTime,building);
+                    //string Name = "Building 5 " + ((int) (pos.x)).ToString() + " " + ((int) (pos.z)).ToString();
                
-                //cityObj[Name] = buildingss;
-                //cityObj.Add(Name,buildingss);
+                    cityObj[tilename] = newBuilding;
+                    //cityObj.Add(Name,buildingss);
+                }
+               
 
                 j++;
                 
