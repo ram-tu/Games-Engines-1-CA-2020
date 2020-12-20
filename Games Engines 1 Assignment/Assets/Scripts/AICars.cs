98c7d96e12cbce7eff3fa77859ac6a11025aeba8
ram-tud <46999658+ram-tu@users.noreply.github.com>
2020-12-20 08:53:32 +0000

    traffic improved

--- Games Engines 1 Assignment/Assets/Scripts/AICars.cs
+++ Games Engines 1 Assignment/Assets/Scripts/AICars.cs
@@ -6,11 +6,15 @@ using UnityEngine;
 public class AICars : MonoBehaviour
 {
     public int speed;
-    public bool front;
-    private int currentSpeed;
+    public bool front; 
+    public int currentSpeed;
+    private GameObject lights;
+
+    public string message;
     // Start is called before the first frame update
     void Start()
     {
+        lights = transform.GetChild(0).gameObject;
         currentSpeed = speed;
     }
 
@@ -19,54 +23,7 @@ public class AICars : MonoBehaviour
     {
         transform.Translate(0,0,speed * Time.deltaTime);
     }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.CompareTag("Car"))
-        {
-            speed = 0;
-        }
-
-        if (front)
-        {
-            if (other.gameObject.CompareTag("Front"))
-            {
-                speed = 0;
-            }
-        }
-
-        if (!front)
-        {
-            if(other.gameObject.CompareTag("Side")){}
-
-            {
-                speed = 0;
-            }
-        }
-        
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.CompareTag("Car"))
-        {
-            speed = currentSpeed;
-        }
-        if (front)
-        {
-            if (other.gameObject.CompareTag("Front"))
-            {
-                speed = currentSpeed;
-            }
-        }
-
-        if (!front)
-        {
-            if(other.gameObject.CompareTag("Side")){}
-
-            {
-                speed = currentSpeed;
-            }
-        }
-    }
+    
+    
+   
 }
---
+++ Games Engines 1 Assignment/Assets/Scripts/CarLights.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarLights : MonoBehaviour
+{
+   
+
+    public AICars car;
+
+    public bool front;
+    // Start is called before the first frame update
+    void Start()
+    {
+        car = transform.parent.gameObject.GetComponent<AICars>();
+        if (car.front)
+        {
+            front = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Car"))
+        {
+            car.speed = 0;
+            car.message = "another car";
+        }
+
+   
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Car"))
+        {
+            car.speed = car.currentSpeed;
+            car.message = "car 2";
+          
+        }
+
+    }
+}
