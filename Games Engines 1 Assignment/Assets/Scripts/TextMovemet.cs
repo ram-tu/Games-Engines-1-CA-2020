b7771c4efee6bef3ffc3c2161e47a6777ca60b5e
ram-tud <46999658+ram-tu@users.noreply.github.com>
2020-12-22 09:18:59 +0000

    main sign

---
+++ Games Engines 1 Assignment/Assets/Scripts/TextMovemet.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class TextMovemet : MonoBehaviour
+{
+    public TMP_Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.ForceMeshUpdate();
+        var textInfo = text.textInfo;
+
+        for (int i = 0; i < textInfo.characterCount; i++)
+        {
+            var character = textInfo.characterInfo[i];
+            if (!character.isVisible)
+            {
+                continue;
+            }
+
+            var verts = textInfo.meshInfo[character.materialReferenceIndex].vertices;
+            for (int j = 0; j < 4; j++)
+            {
+                var orig = verts[character.vertexIndex + j];
+                verts[character.vertexIndex + j] =
+                    orig + new Vector3(0, Mathf.Sin(Time.time * 2f + orig.x * 0.01f) * 10, 0);
+                
+            }
+            
+        }
+
+        for (int i = 0; i < textInfo.meshInfo.Length; i++)
+        {
+            var meshinfo = textInfo.meshInfo[i];
+            meshinfo.mesh.vertices = meshinfo.vertices;
+            text.UpdateGeometry(meshinfo.mesh,i);
+        }
+    }
+}
